fb8dec683b9b7af36059d6ed7fa38aac7361b5b1
tuktuks <pedro.lucas.tukinha25@gmail.com>
2022-11-04 13:07:21 -0300

    Adicionando o highlight

---
+++ src/components/Highlight/stories.tsx
@@ -0,0 +1,30 @@
+import { Story, Meta } from '@storybook/react'
+import Highlight, { HighlightProps } from '.'
+
+export default {
+  title: 'Highlight',
+  component: Highlight,
+  args: {
+    title: "Read Dead it'back",
+    subtitle: "come see John's new adventures",
+    buttonLabel: 'Buy now',
+    buttonLink: 'games/rdr2',
+    backgroundImage: '/img/red-dead-img.jpg'
+  }
+} as Meta
+
+export const Default: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+export const WithFloatImage: Story<HighlightProps> = (args) => (
+  <div style={{ maxWidth: '104rem' }}>
+    <Highlight {...args} />
+  </div>
+)
+
+WithFloatImage.args = {
+  floatImage: '/img/red-dead-float.png'
+}
